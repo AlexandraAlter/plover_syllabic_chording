92f5cfaf00ddc0d99024612d136d34e670579319
AlexandraAlter <alexandra.alter@protonmail.com>
2021-05-09 03:50:54 +0100

    Overhauled the stroke processing system dramatically, using a new system of custom string parsing.

--- plover_velotype/dictionary.py
+++ plover_velotype/dictionary.py
@@ -1,104 +1,160 @@
 """
-
+    Some abbreviations are used in this source file:
+      pref=prefixes
+      suff=suffixes
+      chars=non-prefix and non-suffix keys
+      ic=initial consonants
+      fc=final consonants
+      v=vowels
 """
 
-import codecs
 
-try:
-    import simplejson as json
-except ImportError:
-    import json
-
-from plover.steno_dictionary import StenoDictionary, StenoDictionaryCollection
-from plover.steno import normalize_steno, normalize_stroke
-
-from plover_velotype.system import INIT_CON_KEYS, VOWEL_KEYS, FINAL_CON_KEYS
+from plover import log, system
+from plover.steno_dictionary import StenoDictionaryCollection
+# from plover.steno import normalize_steno, normalize_stroke
+from plover_velotype.system import (
+    META_KEYS,
+    PREFIX_KEYS, PREFIX_META,
+    IC_KEYS, IC_META,
+    V_KEYS, V_META,
+    FC_KEYS, FC_META,
+    SUFFIX_KEYS, SUFFIX_META,
+    IC_V_KEYS, IC_V_META,
+    V_FC_KEYS, V_FC_META,
+    UNDO_STROKE_STENO,
+)
 
 
 class VeloDictionaryCollection:
     def __init__(self, proxy):
         if not isinstance(proxy, StenoDictionaryCollection):
             raise RuntimeError(
-                'Trying to proxy something that isn\'t a StenoDictionaryCollection'
+                "Trying to proxy something that isn't a StenoDictionaryCollection"
             )
         self.proxy = proxy
 
     def __getattr__(self, attr):
         return getattr(self.proxy, attr)
 
-    # abbreviations: ic=initial consonants, fc=final consonants, v=vowels
+    def _extract_space(self, strokes):
+        space_state = SPACE_KEY in strokes[-1]
+        strokes = strokes[:-1] + (strokes[-1].replace(SPACE_KEY, ''),)
+        return strokes, space_state
+
     def _split_stroke(self, stroke):
-        found_vowels = False
-        found_finals = False
-        point1 = len(stroke)
-        point2 = len(stroke)
+        def normalize(stroke):
+            if stroke.endswith('-'):
+                stroke = stroke[:-1]
+            if not stroke:
+                return None
+            return stroke
 
-        for i, c in enumerate(stroke):
-            if not found_vowels and c not in INIT_CON_KEYS:
-                found_vowels = True
-                point1 = i
-            elif found_vowels and c not in VOWEL_KEYS:
-                point2 = i
-                break
+        def slice(stroke, start=0, filter=''):
+            if len(filter) == 0:
+                return None, start
+            f_index = 0
+            for i, c in enumerate(stroke):
+                if i < start:
+                    continue
+                while c != filter[f_index]:
+                    f_index += 1
+                    if f_index >= len(filter):
+                        return normalize(stroke[start:i]), i
+            return normalize(stroke[start:]), len(stroke)
 
-        ic, v, fc = stroke[:point1], stroke[point1:point2], stroke[point2:]
+        # filtered keys
+        # i_* variables represent the end of sections
+        pref, i_pref = slice(stroke, filter=PREFIX_KEYS)
+        ic, i_ic = slice(stroke, start=i_pref, filter=IC_KEYS)
+        v, i_v = slice(stroke, start=i_ic, filter=V_KEYS)
+        fc, i_fc = slice(stroke, start=i_v, filter=FC_KEYS)
+        suff, i_suff = slice(stroke, start=i_fc, filter=SUFFIX_KEYS)
+        ic_v, i_ic_v = slice(stroke, start=i_pref, filter=IC_V_KEYS)
+        v_fc, i_v_fc = slice(stroke, start=i_ic, filter=V_FC_KEYS)
+        keys = normalize(stroke[i_pref:i_fc])
+        # pref = ''.join(k for k in stroke if k in PREFIX_KEYS)
+        # suff = ''.join(k for k in stroke if k in SUFFIX_KEYS)
+        # keys = ''.join(k for k in stroke if k not in PREFIX_KEYS or k not in SUFFIX_KEYS)
+        # ic = ''.join(k for k in left if k in IC_KEYS)
+        # v = ''.join(k for k in keys if k in V_KEYS)
+        # fc = ''.join(k for k in dash + right if k in FC_KEYS)
 
-        norm_ic = ('<',) + normalize_steno(ic) if ic else None
-        norm_v = ('=',) + normalize_steno(v) if v and v != '-' else None
-        norm_fc = ('>',) + normalize_steno('-' + fc) if fc else None
-        norm_ic_v = ('<=',) + normalize_steno(ic + v) if ic and v else None
-        norm_v_fc = ('=>',) + normalize_steno(v + fc) if v and fc else None
+        # qualified keys
+        # blank strokes are also filtered out
+        q_pref = (PREFIX_META, pref) if pref else None
+        q_suff = (SUFFIX_META, suff) if suff else None
+        q_keys = (keys,) if keys else None
+        q_ic = (IC_META, ic) if ic else None
+        q_v = (V_META, v) if v else None
+        q_fc = (FC_META, '-' + fc) if fc else None
+        q_ic_v = (IC_V_META, ic_v) if ic_v else None
+        q_v_fc = (V_FC_META, v_fc) if v_fc else None
 
-        group_l = (norm_ic_v, norm_fc) if norm_ic_v else None
-        group_r = (norm_ic, norm_v_fc) if norm_v_fc else None
-        group_a = (norm_ic, norm_v, norm_fc)
+        # norm_ic = (META_IC,) + normalize_steno(ic) if ic else None
+        # norm_v = (META_V,) + normalize_steno(v) if v and v != '-' else None
+        # norm_fc = (META_FC,) + normalize_steno('-' + fc) if fc else None
+        # norm_ic_v = (META_ICV,) + normalize_steno(ic + v) if ic and v else None
+        # norm_v_fc = (META_VFC,) + normalize_steno(v + fc) if v and fc else None
 
-        return (group_l, group_r, group_a)
+        # grouped keys
+        # all, left, right, each
+        group_a = (q_keys,) if q_keys else None
+        group_l = (q_ic_v, q_fc) if q_ic_v else None
+        group_r = (q_ic, q_v_fc) if q_v_fc else None
+        group_e = (q_ic, q_v, q_fc) if q_ic or q_v or q_fc else None
 
-    def _lookup(self, key, dicts=None, filters=()):
-        def real_lookup(v):
+        return (q_pref, q_suff, (group_a, group_l, group_r, group_e))
+
+    def _generic_lookup(self, lookup_fn, strokes, dicts, filters):
+        if strokes[-1] == UNDO_STROKE_STENO:
+            log.stroke(f'ignoring undo stroke')
+            return None
+
+        if full_match := lookup_fn(strokes):
+            return full_match
+
+        # TODO remove this once multi-stroke support has been added below
+        # or, remove this notice to force split strokes to never use history
+        if len(strokes) > 1:
+            return None
+
+        pref, suff, stroke_groups = self._split_stroke(strokes[-1])
+
+        pref_match = lookup_fn(pref) if pref else ''
+        suff_match = lookup_fn(suff) if suff else ''
+        if pref_match is None or suff_match is None:
+            log.stroke(f'failed to match prefixes {pref} or suffixes {suff}')
+            return None
+
+        for stroke_group in stroke_groups:
+            if not stroke_group:
+                continue
+            matches = list(lookup_fn(s) for s in stroke_group if s)
+            if all(matches):
+                log.stroke(f'split stroke {strokes} into {stroke_group} and matched {matches}')
+                return pref_match + ''.join(matches) + suff_match
+
+
+    def _lookup(self, strokes, dicts=None, filters=()):
+        def lookup_fn(v):
             return self.proxy._lookup(v, dicts=dicts, filters=filters)
 
-        if full_match := real_lookup(key):
-            return full_match
+        return self._generic_lookup(lookup_fn, strokes, dicts, filters)
 
-        if len(key) == 1:
-            stroke_groups = self._split_stroke(key[0])
-
-            for stroke_group in stroke_groups:
-                if not stroke_group or not any(stroke_group):
-                    continue
-                matches = list(real_lookup(s) if s else '{}' for s in stroke_group)
-                print(key, stroke_group, matches)
-                if all(matches):
-                    return ''.join(matches)
-
-    def _lookup_from_all(self, key, dicts=None, filters=()):
-        def real_lookup(v):
+    def _lookup_from_all(self, strokes, dicts=None, filters=()):
+        def lookup_fn(v):
             return self.proxy._lookup_from_all(v, dicts=dicts, filters=filters)
 
-        if full_match := real_lookup(key):
-            return full_match
+        return self._generic_lookup(lookup_fn, strokes, dicts, filters)
 
-        if len(key) == 1:
-            stroke_groups = self._split_stroke(key[0])
+    def lookup(self, strokes):
+        return self._lookup(strokes, filters=self.proxy.filters)
 
-            for stroke_group in stroke_groups:
-                if not stroke_group or not any(stroke_group):
-                    continue
-                matches = list(real_lookup(s) if s else '{}' for s in stroke_group)
-                print(key, stroke_group, matches)
-                if all(matches):
-                    return ''.join(matches)
+    def raw_lookup(self, strokes):
+        return self._lookup(strokes)
 
-    def lookup(self, key):
-        return self._lookup(key, filters=self.proxy.filters)
+    def lookup_from_all(self, strokes):
+        return self._lookup_from_all(strokes, filters=self.proxy.filters)
 
-    def raw_lookup(self, key):
-        return self._lookup(key)
-
-    def lookup_from_all(self, key):
-        return self._lookup_from_all(key, filters=self.proxy.filters)
-
-    def raw_lookup_from_all(self, key):
-        return self._lookup_from_all(key)
+    def raw_lookup_from_all(self, strokes):
+        return self._lookup_from_all(strokes)
--- plover_velotype/system.py
+++ plover_velotype/system.py
@@ -1,8 +1,19 @@
+"""
+    Some abbreviations are used in this source file:
+      ic=initial consonants
+      fc=final consonants
+      v=vowels
+"""
+
 from plover.system import english_stenotype
 
 # ZFSPTCKJR LN H ´ IOE 'UAY# OIE ` NL KJRPTCFSZ _
 
 KEYS = (
+    # Modifier keys
+    '⇧-', '⎈-', '◆-', '❖-', '✦-',
+    # RHS heel
+    '_-',
     # LHS consonants fingers
     'Z-', 'F-', 'S-', 'P-', 'T-', 'C-', 'K-', 'J-', 'R-',
     # LHS consonants thumb
@@ -27,20 +38,34 @@ KEYS = (
     '-N', '-L',
     # RHS consonants fingers
     '-K', '-J', '-R', '-P', '-T', '-C', '-F', '-S', '-Z',
-    # RHS heel
-    '-_',
 )
 
 # not part of the system definition, used in the Velotype Extension
-INIT_CON_KEYS = 'ZFSPTCKJRLNH'
-VOWEL_KEYS = '´IOE-\'UAY#OIE`'
-FINAL_CON_KEYS = 'NLKJRPTCFSZ_'
-META_KEYS = ('<', '=', '>') # these should not be used, as they mark special strokes
+META_KEYS = '«<=>»' # these should not be used, as they mark special strokes
+
+# these MUST be in steno order because of assumptions made in the extension
+PREFIX_KEYS = '⇧⎈◆❖✦_'
+PREFIX_META = '«'
+IC_KEYS = 'ZFSPTCKJRLNH'
+IC_META = '<'
+V_KEYS = "´IOE-'UAY#OIE`"
+V_META = '='
+FC_KEYS = 'NLKJRPTCFSZ'
+FC_META = '>'
+SUFFIX_KEYS = ''
+SUFFIX_META = '»'
+
+IC_V_KEYS = IC_KEYS + V_KEYS
+IC_V_META = IC_META + V_META
+V_FC_KEYS = V_KEYS + FC_KEYS
+V_FC_META = V_META + FC_META
 # end of custom section
 
-IMPLICIT_HYPHEN_KEYS = ("'", 'U', 'A', 'Y', '#', '8', '5', '2', '0')
+# this is intentionally left empty, as it might break the dictionary
+IMPLICIT_HYPHEN_KEYS = ()
 
-SUFFIX_KEYS = ('-_',)
+# this is intentionally left empty
+SUFFIX_KEYS = ()
 
 NUMBER_KEY = '#'
 
@@ -90,10 +115,15 @@ ORTHOGRAPHY_WORDLIST = None
 
 KEYMAPS = {
     'Keyboard': {
+        '⇧-': 'F4',
+        '⎈-': 'F5',
+        '◆-': 'F6',
+        '❖-': 'F7',
+        '✦-': 'F8',
+        '_-': 'space',
         'Z-': 'a',
         'F-': 'w',
         'S-': 's',
-        '#': 'x',
         'P-': '3',
         'T-': 'e',
         'C-': 'd',
@@ -111,6 +141,7 @@ KEYMAPS = {
         'U': 'y',
         'A': 'h',
         'Y': 'n',
+        '#': 'x',
         '-O': '7',
         '-I': 'u',
         '-E': 'j',
@@ -126,11 +157,24 @@ KEYMAPS = {
         '-F': 'p',
         '-S': ';',
         '-Z': "'",
-        '-_': 'space',
         'arpeggiate': 'Return',
-        'no-op': ('`', '1', '2', '0', '-', '=', 'q', '[', ']', '/'),
+        'no-op': ('`', '1', '2', '0', '-', '=', 'q', '[', ']', '/', '\\'),
     },
 }
+
+# mostly for plover_keyboardplus, but harmless otherwise
+# the higher F-keys are useful for binding to other devices, such as footpedals
+KEYMAPS['KeyboardPlus'] = KEYMAPS['Keyboard']
+KEYMAPS['KeyboardPlus'].update({
+    '⇧-': 'F13',
+    '⎈-': 'F17',
+    '◆-': 'F18',
+    '❖-': 'F19',
+    '✦-': 'F20',
+    'H-': ('z', 'F14'),
+    '_-': ('space', 'F15'),
+    '#': ('x', 'F16'),
+})
 
 DICTIONARIES_ROOT = 'asset:plover_velotype:assets'
 DEFAULT_DICTIONARIES = (
